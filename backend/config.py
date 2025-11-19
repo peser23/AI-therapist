8fcaba57b24a63d7e08b493952b072ce28259df9
Praveen Peser <praveenpeser4@gmail.com>
2025-11-18 23:07:08 -0500

    added config with empty keys

---
+++ backend/config.py
@@ -0,0 +1,5 @@
+TWILIO_ACCOUNT_SID = ""
+TWILIO_AUTH_TOKEN = ""
+TWILIO_FROM_NUMBER = "+1112223333"  # your Twilio number
+EMERGENCY_CONTACT = "+7778885555"  # or your local emergency number
+OPENAI_API_KEY = ""
